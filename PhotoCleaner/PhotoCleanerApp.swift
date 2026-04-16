163593e70cb383df6dbcbdb5e57587edf324f784
Daniel Harapiak <daniel@harapiak.com>
2026-04-16 15:48:11 -0400

    Initial Commit

---
+++ PhotoCleaner/PhotoCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoCleanerApp.swift
+//  PhotoCleaner
+//
+//  Created by Daniel Harapiak on 2026-04-16.
+//
+
+import SwiftUI
+
+@main
+struct PhotoCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
